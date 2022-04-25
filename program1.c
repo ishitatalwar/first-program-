385addf2309923d6e930ad4862590d81cba71cd4
ishitatalwar <103492839+ishitatalwar@users.noreply.github.com>
2022-04-25 10:47:48 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("The length of rectangle is \n");
+    scanf("%d",&a);
+    
+    printf("The breadth of rectangle is \n");
+    scanf("%d",&b);
+    
+    printf("The area of the rectangle is %d \n",a*b);
+    return 0;
+}
